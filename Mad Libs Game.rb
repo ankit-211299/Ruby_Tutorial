31562459fc7ef34132aa98a4654ccb3b65e53b92
ankit <ankit.gupta@kreeti.com>
2023-08-07 15:39:00 +0530

    update

---
+++ Mad Libs Game.rb
@@ -0,0 +1,12 @@
+puts "Enter a color: "
+color = gets.chomp();
+
+puts "Enter a prural noun: ";
+prural_noun = gets.chomp();
+
+puts "Enter a celebrity name: ";
+celebrity_name = gets.chomp();
+
+puts ("Roses are "+color)
+puts (prural_noun+" are blue")
+puts ("I love "+celebrity_name);
