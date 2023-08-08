a2ad615ab72ed747521a2f033153a7960b9e8bc1
ankit <ankit.gupta@kreeti.com>
2023-08-08 11:48:49 +0530

    updated

---
+++ hashes_ruby.rb
@@ -0,0 +1,10 @@
+states = {
+    "New York" =>  "NY",
+    "India" => "IND",
+    "Pakistan" => "PAK",
+    "Australia" => "AUS",
+    "South Africa" => "SA"
+};
+
+puts states; #prints the full hash
+puts states["India"]; #prints the value of the mentioned key
---
+++ methods_ruby.rb
@@ -0,0 +1,13 @@
+# def sayhi
+#     puts "Hello User";
+# end
+
+# puts "Top";
+# sayhi
+# puts "Bottom"
+
+def sayhi(name, age)
+    puts ("Hello, "+name+ "you are "+age.to_s)
+end
+
+sayhi("Ankit",23); 
