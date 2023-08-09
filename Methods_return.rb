20564900e4737c932a68c254ef454cab4f25cb25
ankit <ankit.gupta@kreeti.com>
2023-08-09 14:46:08 +0530

    Update

--- Methods_return.rb
+++ Methods_return.rb
@@ -1,8 +1,10 @@
 def cube(num)
 
     return num*num*num,70;
-    # puts "Hello" #will nt get printed
+    # puts "Hello" #will not get printed
 end
 
 puts cube(2);
 
+puts cube(3)[0];
+puts cube(3)[1];
--- hashes_ruby.rb
+++ hashes_ruby.rb
@@ -8,3 +8,40 @@ states = {
 
 puts states; #prints the full hash
 puts states["India"]; #prints the value of the mentioned key
+
+hash_cont = {
+    "January" => "Jan",
+    "Feburary" => "Feb",
+    "March" => "Mar",
+    "April" => "Apr",
+    "May" => "May",
+    "June" => "Jun",
+    "July" => "Jul",
+    "August" => "Aug",
+    "September" => "Sept",
+    "November" => "Nov",
+    "December" => "Dec"
+};
+hash_cont2 = {
+    "January" => "Jan",
+    "Feburary" => "Feb",
+    "March" => "Mar",
+    "April" => "Apr",
+    "May" => "May",
+    "June" => "Jun",
+    "July" => "Jul",
+    "August" => "Aug",
+    "September" => "Sept",
+    "November" => "Nov",
+    "December" => "Dec"
+};
+
+puts hash_cont == hash_cont2 #compares to hashes
+puts hash_cont["January"] = "get" #replaces the value of the key
+puts hash_cont.clear; #clears the hash
+
+# hash_cont2.delete_if {"kk"}
+
+
+puts hash_cont;
+puts hash_cont2;
