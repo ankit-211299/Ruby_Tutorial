7a791588a13c4731f46d9dbb79e350fed71db059
ankit <ankit.gupta@kreeti.com>
2023-08-10 12:58:12 +0530

    update

--- file_read.rb
+++ file_read.rb
@@ -10,9 +10,22 @@ File.open("names_demo.txt", "r") do |file|
     file.close()
 end
 
-puts lines[0]
-puts lines
+# puts lines[0]
+# puts lines
 
-File.open("names_demo.txt", "a") do |file|
-    file.write("\nnew blah from code.")
+# File.open("names_demo.txt", "a") do |file|
+#     file.write("\nnew blah from code.")
+# end
+
+# File.open("names_demo.txt" ,"w") do |file|
+#     file.write("new file")
+# end
+
+File.open("names.txt" ,"w") do |file|
+    file.write ("new file named")
+end
+
+File.open("names_demo.txt", "r+") do |file|
+    file.readline();
+    file.write("new edit")
 end
---
+++ tempCodeRunnerFile.rb
@@ -0,0 +1,3 @@
+e.open("names_demo.txt", "a") do |file|
+#     file.write("\nnew blah from code.")
+# end
