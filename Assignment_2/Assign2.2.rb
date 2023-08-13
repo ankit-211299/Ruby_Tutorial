8a5ac4826eb9d8222a123a8bc9290fd27dfbcac2
ankit <ankit.gupta@kreeti.com>
2023-08-13 10:06:33 +0530

    assignment 2 updated

--- Assignment_2/Assign2.2.rb
+++ Assignment_2/Assign2.2.rb
@@ -4,8 +4,24 @@
     
     Write a program to print it like 
     
-    1-monday, 2-tuesday, 3-wednesdayarr = [1, 5 , 7, 4, 2, 6, 1, 4, 2, 7];
+    1-monday, 2-tuesday, 3-wednesday
+    arr = [1, 5 , 7, 4, 2, 6, 1, 4, 2, 7];
     
     Output: 1-monday, 5-friday, 7-sunday ...
 =end
 
+weekdays = {
+    1 => "Monday",
+    2 => "Tuesday",
+    3 => "Wednesday",
+    4 => "Thursday",
+    5 => "Friday",
+    6 => "Saturday",
+    7 => "Sunday"
+}
+
+arr = [1, 5 , 7, 4, 2, 6, 1, 4, 2, 7]
+
+arr2 = arr.map {|i| weekdays[i]}
+
+puts arr2
